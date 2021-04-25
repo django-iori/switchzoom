c387ee25a45cb24073535d9cfe33e3f5daf1f1f7
shimizu_iori <iori4321999@gmail.com>
2021-04-25 15:57:00 +0900

    third Commit

--- switchzoom/settings.py
+++ switchzoom/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'zoompost.apps.ZoompostConfig',
-    
     'accounts.apps.AccountsConfig',
     'django.contrib.sites',
     'allauth',
