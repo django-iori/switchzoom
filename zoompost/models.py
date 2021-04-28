315c38d10e431677d031259b169bd3260388b063
shimizu_iori <iori4321999@gmail.com>
2021-04-28 11:57:12 +0900

    commit

--- zoompost/models.py
+++ zoompost/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django_mysql.models import ListCharField
 
 class GuestModel(models.Model):
     name = models.CharField(max_length=20, blank=True)
