71c7626d7927bbbf3c22b5ecf9cd8864f5b74dc9
shimizu_iori <iori4321999@gmail.com>
2021-05-04 21:19:25 +0900

    commit

--- switchzoom/settings.py
+++ switchzoom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rq6qya4t=2bb^1@*-1w2mahk**wyeon#d%^n3&kgg_5z!25!ba'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','13.59.177.115']
+ALLOWED_HOSTS = ['13.59.177.115']
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/usr/share/nginx'
+STATIC_ROOT = '/usr/share/nginx/html'
 
 AWS_SES_ACCESS_KEY_ID = os.environ.get('AWS_SES_ACCESS_KEY_ID')
 AWS_SES_SECRET_ACCESS_KEY = os.environ.get('AWS_SES_SECRET_ACCESS_KEY')
