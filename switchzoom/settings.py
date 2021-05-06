4608a219a66d7d7bbb74ceec955c1d057985e009
shimizu_iori <iori4321999@gmail.com>
2021-05-06 15:59:25 +0900

    commit

--- switchzoom/settings.py
+++ switchzoom/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rq6qya4t=2bb^1@*-1w2mahk**wyeon#d%^n3&kgg_5z!25!ba'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','13.59.177.115']
+ALLOWED_HOSTS = []
 
 
 # Application definition
