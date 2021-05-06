81210757ca9f7746290c946059445693c7df4e2c
shimizu_iori <iori4321999@gmail.com>
2021-05-06 17:58:09 +0900

    commit

--- switchzoom/settings.py
+++ switchzoom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rq6qya4t=2bb^1@*-1w2mahk**wyeon#d%^n3&kgg_5z!25!ba'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','13.59.177.115']
+ALLOWED_HOSTS = ['13.59.177.115']
 
 
 # Application definition
